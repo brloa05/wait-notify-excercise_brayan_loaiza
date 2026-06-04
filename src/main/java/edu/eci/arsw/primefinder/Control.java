77095fb243ddb7f3160c3b225a3d747d6243f492
brloa05 <alvarezdairo9@gmail.com>
2026-06-03 22:42:38 -0500

    fix : Se arreglo el problema de concurrencia del codigo.

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -1,44 +1,108 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.primefinder;
 
-/**
- *
- */
+import java.util.Scanner;
+
 public class Control extends Thread {
-    
-    private final static int NTHREADS = 3;
-    private final static int MAXVALUE = 30000000;
-    private final static int TMILISECONDS = 5000;
+
+    private static final int NTHREADS = 3;
+    private static final int MAXVALUE = 30000000;
+    private static final int TMILISECONDS = 5000;
 
     private final int NDATA = MAXVALUE / NTHREADS;
+    private final PrimeFinderThread[] pft;
 
-    private PrimeFinderThread pft[];
-    
+
+    final Object lock = new Object();
+    private boolean paused = false;
+    private int pausedWorkers = 0;
+    private int activeWorkers = NTHREADS;
+
     private Control() {
         super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
-
+        pft = new PrimeFinderThread[NTHREADS];
         int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
-            pft[i] = elem;
+        for (i = 0; i < NTHREADS - 1; i++) {
+            pft[i] = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA, this);
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1, this);
     }
-    
+
     public static Control newControl() {
         return new Control();
     }
 
     @Override
     public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
-            pft[i].start();
+        for (PrimeFinderThread t : pft) t.start();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                Thread.sleep(TMILISECONDS);
+            } catch (InterruptedException e) {
+                break;
+            }
+
+            int active;
+            synchronized (lock) {
+                if (activeWorkers == 0) {
+                    int total = 0;
+                    for (PrimeFinderThread t : pft) total += t.getPrimes().size();
+                    System.out.println("\n[DONE] Total primes found: " + total);
+                    break;
+                }
+
+
+                paused = true;
+                while (pausedWorkers < activeWorkers) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        paused = false;
+                        lock.notifyAll();
+                        return;
+                    }
+                }
+                active = activeWorkers;
+            }
+
+
+            int total = 0;
+            for (PrimeFinderThread t : pft) total += t.getPrimes().size();
+            System.out.println("\n[PAUSED] Primes found so far: " + total);
+
+            if (active > 0) {
+                System.out.println("Press ENTER to resume...");
+                scanner.nextLine();
+                synchronized (lock) {
+                    paused = false;
+                    lock.notifyAll();
+                }
+            } else {
+                System.out.println("All workers finished.");
+                break;
+            }
         }
     }
-    
+
+    void checkPause() throws InterruptedException {
+        synchronized (lock) {
+            while (paused) {
+                pausedWorkers++;
+                lock.notifyAll();
+                lock.wait();
+                pausedWorkers--;
+            }
+        }
+    }
+
+
+    void workerFinished() {
+        synchronized (lock) {
+            activeWorkers--;
+            lock.notifyAll();
+        }
+    }
 }
--- src/main/java/edu/eci/arsw/primefinder/Main.java
+++ src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -2,11 +2,9 @@ package edu.eci.arsw.primefinder;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Control control = Control.newControl();
-        
         control.start();
-
+        control.join();
     }
-	
 }
